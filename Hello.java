8f7ff0bd316b90e6a4557c8325778414d80a5785
Nuwan Kannangara <kannangaranv@gmail.com>
2025-02-08 11:26:08 +0530

    Relational operators

--- Hello.java
+++ Hello.java
@@ -4,8 +4,9 @@ public class Hello {
     
         int num1 = 7;
         int num2 = 3;
-        int result = num1 / num2;
-        System.out.println("Sum of these numbers: "+result);
+        boolean compare = num1 > num2;
+
+        System.out.println(compare);
         
     }
 }
