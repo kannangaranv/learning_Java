5b1b4ccf29b60ef5e1164d2a869c88930e6d3c6b
Nuwan Kannangara <kannangaranv@gmail.com>
2025-02-08 10:30:28 +0530

    Variables in java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+
+        System.out.println("Sum of these numbers: "+sum);
     }
 }
